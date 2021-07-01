2b7aae5a41f29fb5ef0adb8f8f1b7f8ee3d6d9c4
Pigee <haiou511@gmail.com>
2021-07-01 17:51:42 +0800

    second commit

--- main.go
+++ main.go
@@ -1,45 +1,24 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io/ioutil"
-	"os"
+	"github.com/pkg/errors"
 )
-
-type DbConfig struct {
-	Source struct {
-		Db       string `json:"db"`
-		IP       string `json:"ip"`
-		Port     string `json:"port"`
-		User     string `json:"user"`
-		Password string `json:"password"`
-	} `json:"source"`
-	Target struct {
-		Db       string `json:"db"`
-		IP       string `json:"ip"`
-		Port     string `json:"port"`
-		User     string `json:"user"`
-		Password string `json:"password"`
-	} `json:"target"`
-}
 
 func main() {
 
-	jsonFile, err := os.Open("db.json")
-
-	// 最好要处理以下错误
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// 要记得关闭
-	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
+	fmt.Println("Hello.Go..")
 	var dbc DbConfig
-	json.Unmarshal([]byte(byteValue), &dbc)
-	fmt.Println(dbc)
+	var DBS,DBT *sql.DB
+	DBS,DBT = dbc.initcon()
 
+	rows, e := DBS.Query("select t1.id,t3.name AreaName,t1.kh ClientNo,t1.hm ClientName,t2.watermeter_no MeterSeal,t2.address MeterAddress,t5.watert_name FeeKind,now() CreateDate from ys_cust_userbase t1 join ys_cust_watermeter t2 on t1.id = t2.cust_userbase_id join ys_cb_area t3 on t1.area_no = t3.area_no join ys_cust_yspz t4 on t1.id = t4.cust_userbase_id join ys_price_watertype t5 on t4.fyhlx = t5.watert_no limit 1")
+	if e == nil {
+        errors.New("query incur error")
+    }
+	fmt.Println(rows)
+
+	DBS.Close()
+	DBT.Close()
 }
